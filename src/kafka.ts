a500bbf970ee144484f47b52209d9d85ba8f8c8f
Leonardo Anjos <leodrums11@gmail.com>
2023-07-06 01:07:07 -0300

    dividing web application and kafka initialize servers

---
+++ src/kafka.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+        // with docker => host.internal:9092
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+}
+bootstrap();
