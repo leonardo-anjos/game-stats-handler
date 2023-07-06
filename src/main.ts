ca74f93441a7e11a215f024cfba5b8aaac76a4aa
Leonardo Anjos <leodrums11@gmail.com>
2023-07-06 00:09:30 -0300

    kafkajs microservice connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+        // with docker => host.internal:9092
+      },
+    },
+  });
+
+  app.startAllMicroservices();
+
   await app.listen(3000);
 }
 bootstrap();
