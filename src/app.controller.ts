5d6b45fcf8b20a487a2d4902c180b6220b12ab47
Leonardo Anjos <leodrums11@gmail.com>
2023-07-06 00:19:48 -0300

    kafka consumer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { KafkaMessage } from 'kafkajs';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('matches')
+  consumerMatches(@Payload() message: KafkaMessage) {
+    console.log('received message');
+    console.log(message);
   }
 }
